7aa2f7a1fe574dd3d942c57f29de232211f165a8
Marc Mills <mdmills1990@gmail.com>
2018-07-24 16:26:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+function beatlesPlay(musicians,instruments){
+  var playinTheBeatles=[];
+  for (let beatles(0), beatles>4, beatles++){
+  console.log(`${musicians} plays ${instruments}`);}
+  return beatles;
+}
+
+
+function johnLennonFacts(facts){
+  var lennonFacts = []
+  var i = 0
+  while (facts.length > i){
+    lennonFacts.push(`${facts[i]}!!!`)
+    console.log(i++)
+  }
+  return lennonFacts
+  }
+  
+  
+  function iLoveTheBeatles(x){
+  var beatles = []
+  do {
+    beatles.push("I love the Beatles!")
+    x++
+  } while(x < 15)
+  return beatles
+}
+
+  
